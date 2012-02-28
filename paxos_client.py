c619b8daf069b8b759fe9f49ba1f9d93d9d01351
Benjamin Bastian <benjamin.bastian@gmail.com>
2012-02-27 23:12:33 -0800

    Added assertion to paxos client

--- paxos_client.py
+++ paxos_client.py
@@ -15,14 +15,10 @@ class PaxosClient(object):
         self.serialize_message = serialize_message
         self.deserialize_message = deserialize_message
 
-    def request(self, value):
+    def propose(self, value):
+        assert isinstance(value, basestring), \
+                "Proposed value must be of basestring type."
         message = self.serialize_message(self.client_name,
                                          "proposer",
                                          {"message_type": "propose", "value": value})
         self.sock.sendto(message, (self.host, self.port))
-#        response = None
-#        while not response:
-#            raw_response, learner = self.sock.recvfrom(1024)
-#            sender_name, recipient, response_message = self.deserialize_message(raw_response)
-#            if response_message['value'] == value:
-#                return response_message
